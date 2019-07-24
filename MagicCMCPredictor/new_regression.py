0b75503b0fc0cfd5ad1b41c7144178041226db63
Lake <nemquae@gmail.com>
2019-07-24 16:54:26 +0800

    otw - Reading the JSON file now.

--- MagicCMCPredictor/new_regression.py
+++ MagicCMCPredictor/new_regression.py
@@ -4,9 +4,12 @@ URL = "https://archive.scryfall.com/json/scryfall-oracle-cards.json"
 # 17 = CMC
 # 18 = type line
 # 19 = oracle text
-# 20 = colors
-# 21 = color identity
-# 22 = 
+# 20 = power
+# 21 = toughness
+# 22 = colors
+# 23 = color identity
+# 43 = rarity
+
 TARGET_COLUMN = 17
 
 from pandas import read_json
@@ -19,10 +22,123 @@ try:
 except ImportError:
     pass
 
+
+
 def download_data():
 
     frame = read_json(
-        URL
+        URL,
+        orient='columns',
+        dtype='dict'
         )
 
-    return frame[[156, 157, 158, TARGET_COLUMN]]
+    return frame[[18, 19, 20, 21, 22, 23, 43, TARGET_COLUMN]]
+
+
+
+def get_features_and_labels(frame):
+
+    arr = np.array(frame, dtype=np.float)
+
+    from sklearn.preprocessing import StandardScaler, MinMaxScaler
+    arr = MinMaxScaler().fit_transform(arr)
+
+    X, y = arr[:, :-1], arr[:, -1]
+
+    from sklearn.model_selection import train_test_split
+    X_train, _, y_train, _ = train_test_split(X, y, test_size=0.5)
+    X_test, y_test = X, y
+
+    from sklearn.preprocessing import StandardScaler
+    scaler = StandardScaler()
+
+    scaler.fit(X_train)
+    X_train = scaler.transform(X_train)
+    X_test = scaler.transform(X_test)
+
+    return X_train, X_test, y_train, y_test
+
+
+
+def evaluate_learner(X_train, X_test, y_train, y_test):
+
+    # Use a support vector machine for regression
+    from sklearn.svm import SVR
+
+    # Train using a radial basis function
+    svr = SVR(kernel='rbf', gamma=0.1)
+    svr.fit(X_train, y_train)
+    y_pred = svr.predict(X_test)
+    r_2 = svr.score(X_test, y_test)
+    yield 'RBF Model ($R^2={:.3f}$)'.format(r_2), y_test, y_pred
+
+    # Train using a linear kernel
+    svr = SVR(kernel='linear')
+    svr.fit(X_train, y_train)
+    y_pred = svr.predict(X_test)
+    r_2 = svr.score(X_test, y_test)
+    yield 'Linear Model ($R^2={:.3f}$)'.format(r_2), y_test, y_pred
+
+    # Train using a polynomial kernel
+    svr = SVR(kernel='poly', degree=2)
+    svr.fit(X_train, y_train)
+    y_pred = svr.predict(X_test)
+    r_2 = svr.score(X_test, y_test)
+    yield 'Polynomial Model ($R^2={:.3f}$)'.format(r_2), y_test, y_pred
+
+
+
+def plot(results):
+
+    fig, plts = plt.subplots(nrows=len(results), figsize=(8, 8))
+    fig.canvas.set_window_title('Predicting data from ' + URL)
+
+    for subplot, (title, y, y_pred) in zip(plts, results):
+
+        subplot.set_xticklabels(())
+        subplot.set_yticklabels(())
+
+        subplot.set_ylabel('converted mana cost')
+
+        subplot.set_title(title)
+
+        subplot.plot(y, 'b', label='actual')
+        subplot.plot(y_pred, 'r', label='predicted')
+
+        subplot.fill_between(
+            # Generate X values [0, 1, 2, ..., len(y)-2, len(y)-1]
+            np.arange(0, len(y), 1),
+            y,
+            y_pred,
+            color='r',
+            alpha=0.2
+        )
+
+        subplot.axvline(len(y) // 2, linestyle='--', color='0', alpha=0.2)
+
+        subplot.legend()
+
+    fig.tight_layout()
+
+    plt.show()
+
+    plt.close()
+
+
+
+if __name__ == '__main__':
+    # Download the data set from URL
+    print("Downloading data from {}".format(URL))
+    frame = download_data()
+
+    # Process data into feature and label arrays
+    print("Processing {} samples with {} attributes".format(len(frame.index), len(frame.columns)))
+    X_train, X_test, y_train, y_test = get_features_and_labels(frame)
+
+    # Evaluate multiple regression learners on the data
+    print("Evaluating regression learners")
+    results = list(evaluate_learner(X_train, X_test, y_train, y_test))
+
+    # Display the results
+    print("Plotting the results")
+    plot(results)
